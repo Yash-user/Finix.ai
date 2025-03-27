0c24eb5c3e073c37db8601a187f85e69a0be23bd
Yash Bindal <yash.bindal23@gmail.com>
2025-03-27 21:57:56 +0530

    removed extra comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'chat_page.dart';
 import 'educate_page.dart';
 
-void main() {
+// -- CHANGES MADE BY YASH -- //
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
