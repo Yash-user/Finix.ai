2205ed1350da0b8be715a0ceb8411874f5ed3bc2
Samir Gupta <149365632+samir0607@users.noreply.github.com>
2025-03-28 17:19:38 +0530

    Revert "Revert "Integrate backend using FastAPI""

---
+++ lib/backend/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import chatbot
+
+app = FastAPI()
+
+class Message(BaseModel):
+  	message: str
+
+@app.post("/")
+async def send_message(message: Message):
+		response_text = chatbot(message.message)
+		return {"response": response_text}
+
+if __name__ == "__main__":
+		uvicorn.run(app, host='192.168.1.1', port=8000)
