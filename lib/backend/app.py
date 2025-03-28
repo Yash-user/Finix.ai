a8c8f784f7e4eae503316d0347266e3993c5aabf
Nikunj Sharma <nikunjsharma4218@gmail.com>
2025-03-28 17:36:36 +0530

    small fix

--- lib/backend/app.py
+++ lib/backend/app.py
@@ -10,8 +10,8 @@ class Message(BaseModel):
 
 @app.post("/")
 async def send_message(message: Message):
-	response_text = chatbot(message.message)
-	return {"response": response_text}
+    response_text = chatbot(message.message)
+    return {"response": response_text}
 
 if __name__ == "__main__":
-	uvicorn.run(app, host='192.168.1.1', port=8000)
+    uvicorn.run(app, host='192.168.1.1', port=8000)
