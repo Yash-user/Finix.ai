c7648b9ae49c3029b93fae03c993e90351ae796c
samir0607 <samir.gupta987@gmail.com>
2025-03-30 23:42:06 +0530

    chore: refactor backend structure, add stock and news data fetching, and update requirements

--- lib/backend/app.py
+++ lib/backend/app.py
@@ -8,16 +8,8 @@ app = FastAPI()
 class Message(BaseModel):
     message: str
 
-
-@app.get("/")
-async def test():
-    return {"response": "hello"}
-
-
 @app.post("/")
 async def send_message(message: Message):
     response_text = chatbot(message.message)
     return {"response": response_text}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
