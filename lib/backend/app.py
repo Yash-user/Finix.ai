631f4d04ee7f8c0bd919747dcd0a6a6a8c3526af
samir0607 <samir.gupta987@gmail.com>
2025-04-01 23:25:24 +0530

    updated file

--- lib/backend/app.py
+++ lib/backend/app.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import uvicorn
 from chatbot import chatbot
 
 app = FastAPI()
 
 class Message(BaseModel):
     message: str
+    category: int
 
 @app.post("/")
-async def send_message(message: Message):
-    response_text = chatbot(message.message)
+async def send_message(message: Message, category: int):
+    response_text = chatbot(message.message, category.category)
     return {"response": response_text}
-
